a1e6aa5907b904810cefe6e02a97d8dabef7014d
karan-tomar <karan.tomar_cs19@gla.ac.in>
2020-08-30 14:27:54 +0530

    create Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -11,6 +11,11 @@ public class Library {
     private Book[] availableBooks;
 
     public Library() {
+        this.availableBooks = new Book[MAXIMUM_BOOKS_IN_LIBRARY];
+        for (int i = 0; i < MAXIMUM_BOOKS_IN_LIBRARY; i++) {
+            availableBooks[i] = new Book();
+
+        }
 
     }
 }
